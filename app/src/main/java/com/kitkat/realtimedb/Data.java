bcb9d8cec8a9ec287667c63748c1c49ad5e81ca0
Loveleen Kaur <loveleen.nancy@gmail.com>
2020-07-25 00:44:20 +0530

    Initial commit

---
+++ app/src/main/java/com/kitkat/realtimedb/Data.java
@@ -0,0 +1,33 @@
+package com.kitkat.realtimedb;
+
+public class Data {
+
+    String key;
+
+    String name;
+    int age;
+
+    public Data(String key, String name, int age) {
+        this.key = key;
+        this.name = name;
+
+        this.age = age;
+    }
+
+    public Data() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+}
